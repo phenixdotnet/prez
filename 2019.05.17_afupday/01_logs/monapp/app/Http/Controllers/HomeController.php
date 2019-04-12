ad83e8ced2b9647e447c3e80c1d6d96e8829143b
Vincent Lainé <vincent@kelbillet.com>
2019-04-12 15:47:11 +0200

    Added afupday prez

---
+++ 2019.05.17_afupday/01_logs/monapp/app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Log;
+
+class HomeController extends Controller
+{
+    public function show()
+    {
+        Log::info("call HomeController");
+        return view('welcome');
+    }
+}
+
