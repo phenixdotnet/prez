a789d314b0fe57f0871ecfa78215cf6d4ba37add
Vincent Lainé <vincent@kelbillet.com>
2019-05-05 17:09:28 +0200

    Update afup day prez

--- 2019.05.17_afupday/01_config/create_configmap.sh
+++ 2019.05.17_afupday/01_config/create_configmap.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+kubectl delete configmap monapp-config-env-file
 kubectl create configmap monapp-config-env-file --from-env-file=monapp_config/prod/.env
+
+kubectl delete configmap monapp-config-consul-env-file
+kubectl create configmap monapp-config-consul-env-file --from-env-file=monapp_config/prod_consul/.env
