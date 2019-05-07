f9e5f170e30de3bc4401b91c8b08b7254d8b6c6a
Vincent Lainé <vincent@kelbillet.com>
2019-05-07 17:31:38 +0200

    Added logs support

--- 2019.05.17_afupday/k8s_monapp/create_configmap.sh
+++ 2019.05.17_afupday/k8s_monapp/create_configmap.sh
@@ -5,3 +5,6 @@ kubectl create configmap monapp-config-env-file --from-env-file=monapp_config/pr
 
 kubectl delete configmap monapp-config-consul-env-file
 kubectl create configmap monapp-config-consul-env-file --from-env-file=monapp_config/prod_consul/.env
+
+kubectl delete configmap monapp-filebeat-configmap
+kubectl create configmap monapp-filebeat-configmap --from-file=./filebeat
--- 2019.05.17_afupday/monapp/app/Http/Controllers/HomeController.php
+++ 2019.05.17_afupday/monapp/app/Http/Controllers/HomeController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
     public function show() {
 
+
         $results = DB::select("SELECT text FROM texts");
         $text = $results[0]->text;
+
+        Log::info("Loaded text for home page", ["text" => $text]);
 
         return view('welcome', [
             'AppName' => config("app.name"),
